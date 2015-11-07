14dfa0d7dd43f03d023d1b2df9c98d411f934237
Michael Kapelko <kornerr@gmail.com>
2015-11-07 18:04:28 +0700

    #2310 Construct ideally short WAM scripts - SingleTarget perfected

--- scripts/SingleTarget.py
+++ scripts/SingleTarget.py
@@ -7,29 +7,30 @@ TARGET_ACTION_WAIT = "delay.default.waitForLeverage"
 class Target:
     def __init__(self, sceneName, nodeName, env):
         # Refer.
-        self.env = env
+        self.env       = env
+        self.sceneName = sceneName
+        self.nodeName  = nodeName
         # Create.
         self.u = EnvironmentUser("Target", "Turn specific node into WAM target")
         self.isMoving = False
         # Listen to node selection.
-        key = ["selector", sceneName, "selectedNode"]
-        self.u.listen(key, nodeName, self, "onSelection")
+        self.u.listen(["selector", sceneName, "selectedNode"]
+                      nodeName,
+                      self.onSelection)
         # Listen to pop action finish.
-        key = [TARGET_ACTION_POP, sceneName, nodeName, "active"]
-        self.u.listen(key, "0", self, "onPopFinished")
+        self.u.listen([TARGET_ACTION_POP, sceneName, nodeName, "active"],
+                      "0",
+                      self.onPopFinished)
         # Listen to wait action.
-        key = [TARGET_ACTION_WAIT, sceneName, nodeName, "active"]
-        self.u.listen(key, None, self, "onWait")
-        # Provide "catch" ability.
-        key = ["target", sceneName, nodeName, "catch"]
-        # Since no class and methods are specified, this property is only reported.
-        self.u.provide(key)
-        # Provide "moving" ability. Only setter.
-        key = ["target", sceneName, nodeName, "moving"]
-        self.u.provide(key, self, "setMoving")
-        # Provide "selected" ability. Only reporting.
-        key = ["target", sceneName, nodeName, "selected"]
-        self.u.provide(key)
+        self.u.listen([TARGET_ACTION_WAIT, sceneName, nodeName, "active"],
+                      None,
+                      self.onWait)
+        # Report "catch".
+        self.u.provide(self.property("catch"))
+        # Accept "moving".
+        self.u.provide(self.property("moving"), self.setMoving)
+        # Report "selected".
+        self.u.provide(self.property("selected"))
         # Prepare.
         self.env.registerUser(self.u)
     def __del__(self):
@@ -41,26 +42,21 @@ class Target:
         self.env = None
     def onPopFinished(self, key, value):
         self.isMoving = False
-        key = ["target", sceneName, nodeName, "moving"]
-        self.u.set(key, "0")
+        self.u.set(self.property("moving"), "0")
     def onSelection(self, key, value):
-        # Ignore non-moving.
+        # Ignore selection if we're not moving.
         if (not self.isMoving):
             return
-        sceneName = key[1]
-        nodeName  = value[0]
-        key = ["target", sceneName, nodeName, "selected"]
-        self.u.set(key, "1")
-        self.u.set(key, "0")
+        self.u.set(self.property("selected"), "1")
+        self.u.set(self.property("selected"), "0")
     def onWait(self, key, value):
-        key = ["target", sceneName, nodeName, "catch"]
-        self.u.set(key, value[0])
+        self.u.set(self.property("catch"), value[0])
+    def property(self, name):
+        return ["target", self.sceneName, self.nodeName, name]
     def setMoving(self, key, value):
-        sceneName = key[1]
-        nodeName  = key[2]
         # Assume setMoving is only called with value = 1.
         self.isMoving = True
-        key = [TARGET_ACTION_POP, sceneName, nodeName, "active"]
+        key = [TARGET_ACTION_POP, self.sceneName, self.nodeName, "active"]
         self.u.set(key, "1")
 
 def SCRIPT_CREATE(sceneName, nodeName, env):
@@ -68,3 +64,4 @@ def SCRIPT_CREATE(sceneName, nodeName, env):
 
 def SCRIPT_DESTROY(instance):
     del instance
+
