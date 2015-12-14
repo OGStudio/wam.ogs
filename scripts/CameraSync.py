53fa98595d0f2188c443112c2997b8b11e454e68
Michael Kapelko <kornerr@gmail.com>
2015-12-14 22:10:47 +0700

    #2317 Player - Camera sync

--- scripts/CameraSync.py
+++ scripts/CameraSync.py
@@ -8,12 +8,23 @@ class CameraSyncImpl(object):
     def __del__(self):
         # Derefer.
         self.c = None
+    def cameraToNode(self):
+        pos = self.c.get("camera.position")[0]
+        rot = self.c.get("camera.rotation")[0]
+        self.c.set("node.$SCENE.$NODE.position", pos)
+        self.c.set("node.$SCENE.$NODE.rotation", rot)
     def nodeToCamera(self):
-        print "nodeToCamera"
         pos = self.c.get("node.$SCENE.$NODE.position")[0]
         rot = self.c.get("node.$SCENE.$NODE.rotation")[0]
         self.c.set("camera.position", pos)
         self.c.set("camera.rotation", rot)
+    def setSync(self, key, value):
+        print "setSync", key, value
+        src = value[0]
+        if (src == "node"):
+            self.nodeToCamera()
+        else:
+            self.cameraToNode()
 
 class CameraSync(object):
     def __init__(self, sceneName, nodeName, env):
@@ -24,6 +35,7 @@ class CameraSync(object):
         self.c.setConst("SCENE", sceneName)
         self.c.setConst("NODE",  nodeName)
         self.impl.nodeToCamera()
+        self.c.provide("camera.sync", self.impl.setSync)
         print "{0} CameraSync.__init__({1}, {2})".format(id(self), sceneName, nodeName)
     def __del__(self):
         # Tear down.
