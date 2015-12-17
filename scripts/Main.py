eadc3c552f347057e947fcff7086e1670cbabf80
Michael Kapelko <kornerr@gmail.com>
2015-12-17 10:34:34 +0700

    #2317 Player - Target stub

--- scripts/Main.py
+++ scripts/Main.py
@@ -1,37 +1,35 @@
 
-import pymjin2
+from pymjin2 import *
+import random
+
+SINGLE_MAIN_TARGETS_NB         = 5
+SINGLE_MAIN_TARGET_NAME        = "target"
+SINGLE_MAIN_LEVERAGE_NAME      = "leverage"
 
 class MainImpl(object):
     def __init__(self, client):
         # Refer.
         self.c = client
-        # Create.
-        self.mousePos = None
     def __del__(self):
         # Derefer.
         self.c = None
-    def onMousePosition(self, key, value):
-        self.mousePos = value[0]
-    def onNodeSelection(self, key, value):
-        print "onNodeSelection", key, value
-    def onNodeSelectionShortcut(self, key, value):
-        self.c.set("selector.$SCENE.position", self.mousePos)
+    def onFinishedLoading(self, key, value):
+        print "Starting the game"
+        self.popRandomTarget()
+    def popRandomTarget(self):
+        random.seed(rand(True))
+        id = random.randint(1, SINGLE_MAIN_TARGETS_NB)
+        self.c.setConst("NODE", SINGLE_MAIN_TARGET_NAME + str(id))
+        self.c.set("target.$SCENE.$NODE.moving", "1")
 
 class Main(object):
     def __init__(self, sceneName, nodeName, env):
         # Create.
-        self.c    = pymjin2.EnvironmentClient(env, "Main")
+        self.c    = EnvironmentClient(env, "Main")
         self.impl = MainImpl(self.c)
         # Prepare.
         self.c.setConst("SCENE", sceneName)
-        # Listen to node selection shortcut.
-        self.c.listen("shortcut.node.select.active",
-                      "1",
-                      self.impl.onNodeSelectionShortcut)
-        # Listen to mouse position.
-        self.c.listen("mouse.positionOrig", None, self.impl.onMousePosition)
-        # Listen to target selection.
-        self.c.listen("node.$SCENE..selected", "1", self.impl.onNodeSelection)
+        self.c.listen("scene.opened", None, self.impl.onFinishedLoading)
         print "{0} Main.__init__({1}, {2})".format(id(self), sceneName, nodeName)
     def __del__(self):
         # Tear down.
--- scripts/Target.py
+++ scripts/Target.py
@@ -1,148 +1,49 @@
 
-import pymjin2
+from pymjin2 import *
 
-TARGET_ACTION_GROUP     = "default"
-TARGET_ACTION_TYPE_WAIT = "delay"
-TARGET_ACTION_TYPE_POP  = "sequence"
-TARGET_ACTION_NAME_WAIT = "waitForLeverage"
-TARGET_ACTION_NAME_POP  = "popTarget"
-
-class TargetState(object):
-    def __init__(self):
-        self.wait   = None
-        self.pop    = None
-        self.moving = False
-    def setActionGroup(self, groupName):
-        self.wait = "{0}.{1}.{2}".format(TARGET_ACTION_TYPE_WAIT,
-                                         groupName,
-                                         TARGET_ACTION_NAME_WAIT)
-        self.pop = "{0}.{1}.{2}".format(TARGET_ACTION_TYPE_POP,
-                                        groupName,
-                                        TARGET_ACTION_NAME_POP)
+TARGET_ACTION_POP  = "sequence.default.popTarget"
+TARGET_ACTION_WAIT = "delay.default.waitForLeverage"
 
 class TargetImpl(object):
-    def __init__(self, scene, action, senv):
+    def __init__(self, client):
         # Refer.
-        self.scene  = scene
-        self.action = action
-        self.senv   = senv
+        self.c = client
         # Create.
-        self.selectable = {}
-        self.actions    = {}
+        self.isMoving = False
     def __del__(self):
         # Derefer.
-        self.scene  = None
-        self.action = None
-        self.senv   = None
-    def onActionState(self, key, values):
-        state = (values[0] == "1")
-        # Ignore other actions.
-        actionName = key.replace(".active", "")
-        if (actionName not in self.actions):
-            return
-        node = self.actions[actionName]
-        s = self.selectable[node]
-        if (actionName == s.pop):
-            # Ignore activation.
-            if (state):
-                return
-            s.moving = False
-            self.report(node, "moving", "0")
-        elif (actionName == s.wait):
-            self.report(node, "catch", values[0])
-    def onSelection(self, key, values):
-        nodeName = values[0]
-        # Ignore deselection.
-        if (not len(nodeName)):
-            return
-        v = key.split(".")
-        sceneName = v[1]
-        node = sceneName + "." + nodeName
-        if (node not in self.selectable):
-            return
-        s = self.selectable[node]
-        # Ignore non-moving.
-        if (not s.moving):
-            return
-        self.report(node, "selected", "1")
-        self.report(node, "selected", "0")
-    def report(self, node, property, value):
-        st = pymjin2.State()
-        key = "target.{0}.{1}".format(node, property)
-        st.set(key, value)
-        self.senv.reportStateChange(st)
-    def setCatch(self, key, values):
-        print "setCatch", key, values
-    def setMoving(self, key, values):
-        v = key.split(".")
-        sceneName = v[1]
-        nodeName  = v[2]
-        node = sceneName + "." + nodeName
-        if (node not in self.selectable):
-            return
-        s = self.selectable[node]
-        s.moving = True
-        st = pymjin2.State()
-        st.set("{0}.node".format(s.pop), node)
-        st.set("{0}.active".format(s.pop), "1")
-        self.action.setState(st)
-    def setSelectable(self, key, values):
-        v = key.split(".")
-        sceneName = v[1]
-        nodeName  = v[2]
-        state     = (values[0] == "1")
-        node = sceneName + "." + nodeName
-        if (state):
-            # Clone sequence action and its children.
-            key = "{0}.{1}.{2}.clone".format(TARGET_ACTION_TYPE_POP,
-                                             TARGET_ACTION_GROUP,
-                                             TARGET_ACTION_NAME_POP)
-            st = self.action.state([key])
-            newGroupName = st.value(key)[0]
-            s = TargetState()
-            s.setActionGroup(newGroupName)
-            self.selectable[node] = s
-            self.actions[s.wait] = node
-            self.actions[s.pop]  = node
-        # Remove disabled.
-        elif (node in self.selectable):
-            s = self.selectable[node]
-            del self.actions[s.wait]
-            del self.actions[s.pop]
-            del self.selectable[node]
-    def setSelected(self, key, values):
-        print "setSelected", key, values
+        self.c = None
+    def onSelection(self, key, value):
+        print "onSelection", key, value
+    def setMoving(self, key, value):
+        print "setMoving", key, value
+        self.isMoving = True
+        self.c.set("$POP.$SCENE.$NODE.active", "1")
 
-class Target:
-    def __init__(self, scene, action, scriptEnvironment):
+class Target(object):
+    def __init__(self, sceneName, nodeName, env):
         # Create.
-        self.impl = TargetImpl(scene, action, scriptEnvironment)
-        self.subs = pymjin2.Subscriber()
-        self.prov = pymjin2.Provider(scriptEnvironment,
-                                     "Target",
-                                     "Turn any node into WAM target")
+        name      = "Target/{0}/{1}".format(sceneName, nodeName)
+        self.c    = EnvironmentClient(env, name)
+        self.impl = TargetImpl(self.c)
         # Prepare.
-        # Listen to node selection.
-        key = "selector..selectedNode"
-        self.subs.subscribe(scene, key, self.impl, "onSelection")
-        # Listen to target pop state.
-        key = "{0}..{1}.active".format(TARGET_ACTION_TYPE_POP,
-                                       TARGET_ACTION_NAME_POP)
-        self.subs.subscribe(action, key, self.impl, "onActionState")
-        # Listen to target delay state.
-        key = "{0}..{1}.active".format(TARGET_ACTION_TYPE_WAIT,
-                                       TARGET_ACTION_NAME_WAIT)
-        self.subs.subscribe(action, key, self.impl, "onActionState")
-        # Provide "Target" API.
-        self.prov.provide("target...catch",      self.impl, "setCatch")
-        self.prov.provide("target...moving",     self.impl, "setMoving")
-        self.prov.provide("target...selectable", self.impl, "setSelectable")
-        self.prov.provide("target...selected",   self.impl, "setSelected")
-        print "{0} Target.__init__".format(id(self))
+        self.c.setConst("SCENE", sceneName)
+        self.c.setConst("NODE",  nodeName)
+        self.c.setConst("POP",   TARGET_ACTION_POP)
+        self.c.setConst("WAIT",  TARGET_ACTION_WAIT)
+        # Listen to target selection.
+        self.c.listen("node.$SCENE.$NODE.selected", "1", self.impl.onSelection)
+        self.c.provide("target.$SCENE.$NODE.moving", self.impl.setMoving)
     def __del__(self):
-        # Destroy.
-        del self.subs
-        del self.prov
+        # Tear down.
+        self.c.clear()
+        # Destroy
         del self.impl
-        print "{0} Target.__del__".format(id(self))
+        del self.c
+
+def SCRIPT_CREATE(sceneName, nodeName, env):
+    return Target(sceneName, nodeName, env)
+
+def SCRIPT_DESTROY(instance):
+    del instance
 
